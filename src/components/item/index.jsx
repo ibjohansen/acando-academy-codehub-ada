00222f85d2bcbea941d22ab0da0f613bd07def72
Ib Johansen <ib.johansen.jr@gmail.com>
2017-10-30 13:37:05 +0100

    removed most code on list and item

--- src/components/item/index.jsx
+++ src/components/item/index.jsx
@@ -1,159 +1,23 @@
 import React from 'react';
-import isEqual from 'lodash/isEqual';
 
 export default class Person extends React.Component {
   constructor() {
     super();
-    this.state = {
-      editing: false,
-      person: {},
-      preEditPerson: {},
-    };
-    this.onToggleEditOn = this.onToggleEditOn.bind(this);
-    this.onToggleEditOff = this.onToggleEditOff.bind(this);
-    this.onToggleEditAbort = this.onToggleEditAbort.bind(this);
-    this.onPostEdit = this.onPostEdit.bind(this);
-    this.onDeletePerson = this.onDeletePerson.bind(this);
-    this.onChangeField = this.onChangeField.bind(this);
-    this.onShowCamClick = this.onShowCamClick.bind(this);
-    this.updatePerson = this.updatePerson.bind(this);
-  }
-
-  componentWillMount() {
-    this.updatePerson(this.props.person);
-  }
-
-  componentWillReceiveProps(nextProps) {
-    this.updatePerson(nextProps.person);
-  }
-
-  onToggleEditOn() {
-    this.setState({
-      editing: true,
-      preEditPerson: Object.assign({}, this.state.person)
-    });
-  }
-
-  onToggleEditOff() {
-    this.setState({
-      editing: false,
-      preEditPerson: {}
-    });
-  }
-
-  onToggleEditAbort() {
-    if (this.isEdited()) {
-      this.setState({
-        editing: false,
-        person: this.state.preEditPerson,
-        preEditPerson: {}
-      });
-    }
-  }
-
-  onChangeField(e) {
-    const key = e.target.name;
-    const val = e.target.value;
-    const state = Object.assign({}, this.state);
-    state.person[key] = val;
-    this.setState(state);
-  }
-
-  onDeletePerson() {
-    const {person} = this.state;
-    return fetch(`/api/people/${person.key}`, {
-      method: 'DELETE',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json'
-      }
-    })
-      .then(() => {
-        this.props.onUpdateList();
-      })
-      .catch((error) => {
-        console.error(error); // eslint-disable-line no-console
-        this.props.onUpdateList();
-      });
-  }
-
-  onPostEdit() {
-    const {person} = this.state;
-    return fetch(`/api/people/${person.key}`, {
-      method: 'PUT',
-      body: JSON.stringify(person),
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json'
-      }
-    })
-      .then(() => {
-        this.onToggleEditOff();
-      })
-      .catch((error) => {
-        console.error(error); // eslint-disable-line no-console
-        this.onToggleEditOff();
-      });
-  }
-
-  onShowCamClick(e) {
-    this.props.onShowCamClickCallback(e.target.getAttribute('id'));
-  }
-
-  updatePerson(person) {
-    this.setState({
-      person
-    });
-  }
-
-  isEdited() {
-    return this.state.editing && !isEqual(this.state.person, this.state.preEditPerson);
+    this.state = {};
   }
 
   render() {
-    const {person} = this.state;
-    const id = person.key;
-    const menuClasses = 'row menu';
     return (
       <div className="container person">
         <div className="row">
           <div className="column column-75">
             <div>
-              { /* PERSONKORT HER */ }
+              {/* PERSONKORT HER */}
             </div>
           </div>
           <div className="column column-25">
-            { /* BILDE HER */ }
+            {/* BILDE HER */}
           </div>
-        </div>
-        <div className={menuClasses}>
-          <button
-            className="button button-outline"
-            disabled={!this.isEdited()}
-            onClick={this.onToggleEditAbort}
-            id={id}
-          >avbryt
-          </button>
-          <button
-            className="button button-outline"
-            disabled={!this.isEdited()}
-            onClick={this.onPostEdit}
-            id={id}
-          >lagre
-          </button>
-          <button
-            className="button button-outline"
-            onClick={this.onShowCamClick}
-            id={id}
-          >bytt bilde
-          </button>
-          <button
-            className="button button-outline"
-            onClick={this.onDeletePerson}
-            id={id}
-          >slett
-          </button>
-
         </div>
       </div>
     );
--- src/components/list/index.jsx
+++ src/components/list/index.jsx
@@ -1,45 +1,9 @@
 import React from 'react';
-import Person from '../item';
 
 export default class List extends React.Component {
   constructor() {
     super();
-    this.state = {
-      data: {}
-    };
-    this.onShowCamClickCallback = this.onShowCamClickCallback.bind(this);
-    this.onHideCamClickCallback = this.onHideCamClickCallback.bind(this);
-    this.onUpdateList = this.onUpdateList.bind(this);
-  }
-
-  componentDidMount() {
-    this.fetchPeople();
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.updateList) {
-      this.fetchPeople();
-    }
-  }
-
-  onShowCamClickCallback(id) {
-    this.props.showCamCallback(id);
-  }
-
-  onHideCamClickCallback() {
-    this.props.hideCamCallback();
-  }
-
-  onUpdateList() {
-    this.fetchPeople();
-  }
-
-  fetchPeople() {
-    fetch('/api/people').then((data) => data.json()).then((json) => {
-      this.setState({
-        data: json
-      });
-    });
+    this.state = {};
   }
 
   render() {
